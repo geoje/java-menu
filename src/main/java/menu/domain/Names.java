2e6ccad20015e776eecc63407144d28458681844
Gyeongho Yang <chchch1213@naver.com>
2023-12-13 21:25:18 +0900

    refactor(domain): 이름 리스트를 받고 구분할 때의 구분자 수정

--- src/main/java/menu/domain/Names.java
+++ src/main/java/menu/domain/Names.java
@@ -1,14 +1,13 @@
 package menu.domain;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static menu.constant.ErrorMessage.*;
 
 public record Names(List<Name> names) {
+    private static final String DELIMITER = ",";
     private static final int MIN_COUNT = 2;
     private static final int MAX_COUNT = 5;
 
@@ -18,7 +17,7 @@ public record Names(List<Name> names) {
     }
 
     public static Names from(String names) {
-        return new Names(Arrays.stream(names.split(names)).map(Name::new).collect(Collectors.toList()));
+        return new Names(Arrays.stream(names.split(DELIMITER)).map(Name::new).collect(Collectors.toList()));
     }
 
     private static void validateCount(List<Name> names) {
@@ -30,8 +29,8 @@ public record Names(List<Name> names) {
         }
     }
 
-    private static void validateNotDuplicate(List<Name> names) throws IllegalArgumentException {
-        if (new HashSet<>(Collections.singletonList(names)).size() != names.size()) {
+    private static void validateNotDuplicate(List<Name> names) {
+        if (names.stream().map(Name::name).collect(Collectors.toSet()).size() != names.size()) {
             throw new IllegalArgumentException(NAME_DUPLICATED.toString());
         }
     }
