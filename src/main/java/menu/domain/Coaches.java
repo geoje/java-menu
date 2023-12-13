dddd970860f43f137d41737b793c91ddc564dea6
Gyeongho Yang <chchch1213@naver.com>
2023-12-14 08:07:30 +0900

    feat(domain): 코치들과 요일별 카테고리를 가지고 있으며 점심 추첨을 돌리는 함수가 있는 Coaches record 생성

--- src/main/java/menu/domain/Coaches.java
+++ src/main/java/menu/domain/Coaches.java
@@ -1,6 +1,49 @@
 package menu.domain;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import menu.constant.Menu;
+import menu.constant.MenuType;
 
-public record Coaches(List<Coach> coaches) {
+import java.time.DayOfWeek;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.time.DayOfWeek.*;
+
+public class Coaches {
+    private static final List<DayOfWeek> WEEKDAYS = List.of(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY);
+    private final List<Coach> coaches;
+    private final MenuType[] categories;
+
+    public Coaches(List<Coach> coaches) {
+        this.coaches = coaches;
+        categories = new MenuType[DayOfWeek.values().length + 1];
+    }
+
+    public void calculateLunches() {
+        WEEKDAYS.forEach(dayOfWeek -> {
+            MenuType type = MenuType.values()[Randoms.pickNumberInRange(1, 5) - 1];
+            categories[dayOfWeek.getValue()] = type;
+
+            coaches.forEach(coach -> {
+                List<Menu> exclude = Stream.of(coach.getUneatable().menus(), coach.getWeekdayLunches())
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toList());
+                List<String> choice = Menu.getAllByTypeWithoutSome(type, exclude).stream()
+                        .map(Menu::getName)
+                        .toList();
+                coach.setLunches(dayOfWeek, Menu.getByName(Randoms.shuffle(choice).get(0)).get());
+            });
+        });
+    }
+
+    public List<Coach> getCoaches() {
+        return coaches;
+    }
+
+    public List<MenuType> getCategories() {
+        return WEEKDAYS.stream().map(dayOfWeek -> categories[dayOfWeek.getValue()]).toList();
+    }
 }
