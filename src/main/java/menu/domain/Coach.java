d801b67b0627d24584fd49c9ad9dd6165989fba3
Gyeongho Yang <chchch1213@naver.com>
2023-12-14 08:06:45 +0900

    feat(domain): 이름과 못먹는 메뉴 및 점심메뉴를 가지고 있는 Coach record 생성

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,5 +1,37 @@
 package menu.domain;
 
-public record Coach(Name name, Menus uneatable) {
-    
+import menu.constant.Menu;
+
+import java.time.DayOfWeek;
+import java.util.List;
+
+import static java.time.DayOfWeek.*;
+
+public class Coach {
+    private static final List<DayOfWeek> WEEKDAYS = List.of(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY);
+    private final Name name;
+    private final Menus uneatable;
+    private final Menu[] lunches;
+
+    public Coach(Name name, Menus uneatable) {
+        this.name = name;
+        this.uneatable = uneatable;
+        lunches = new Menu[DayOfWeek.values().length];
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Menus getUneatable() {
+        return uneatable;
+    }
+
+    public List<Menu> getWeekdayLunches() {
+        return WEEKDAYS.stream().map(dayOfWeek -> lunches[dayOfWeek.getValue()]).toList();
+    }
+
+    public void setLunches(DayOfWeek dayOfWeek, Menu lunch) {
+        lunches[dayOfWeek.getValue()] = lunch;
+    }
 }
