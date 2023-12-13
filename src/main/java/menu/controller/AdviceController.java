b210e5c01554b121d98d0d67faba7789f516ca7f
Gyeongho Yang <chchch1213@naver.com>
2023-12-13 22:52:48 +0900

    feat(controller): 최종 결과 출력까지 상수로 임시 구현

--- src/main/java/menu/controller/AdviceController.java
+++ src/main/java/menu/controller/AdviceController.java
@@ -1,16 +1,18 @@
 package menu.controller;
 
-import menu.constant.GeneralMessage;
-import menu.domain.Names;
+import menu.domain.*;
 import menu.view.InputView;
 import menu.view.OutputView;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class AdviceController {
     public static void run() {
         notifyStart();
         Names names = requestNames();
+        Coaches coaches = requestCoached(names);
+        notifyResult(coaches);
     }
 
     private static <T> T requestUntilValidated(Supplier<T> supplier) {
@@ -24,10 +26,29 @@ public class AdviceController {
     }
 
     private static void notifyStart() {
-        OutputView.printGeneralMessage(GeneralMessage.NOTIFY_START);
+        OutputView.printStartTitle();
+    }
+
+    private static void notifyResult(Coaches coaches) {
+        OutputView.printResultTitle();
+        OutputView.printCategoryMessage("한식", "양식", "일식", "중식", "아시안");
+        OutputView.printAdviceMessage("구구", "김치찌개", "스파게티", "규동", "짜장면", "카오 팟");
+        OutputView.printAdviceMessage("제임스", "제육볶음", "라자냐", "가츠동", "짬뽕", "파인애플 볶음밥");
+        OutputView.printFinishTitle();
     }
 
     private static Names requestNames() {
         return requestUntilValidated(() -> Names.from(InputView.readNames()));
+    }
+
+    private static Menus requestUneatable(Name name) {
+        return requestUntilValidated(() -> Menus.from(InputView.readUneatable(name.name())));
+    }
+
+    private static Coaches requestCoached(Names names) {
+        List<Coach> coaches = names.names().stream()
+                .map(name -> new Coach(name, requestUneatable(name)))
+                .toList();
+        return new Coaches(coaches);
     }
 }
