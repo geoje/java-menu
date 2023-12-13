d7f8afab92f308d6479de43bcff293443467af37
Gyeongho Yang <chchch1213@naver.com>
2023-12-13 22:48:55 +0900

    feat(const): 출력 및 에러 메세지 추가 및 변경

--- src/main/java/menu/constant/ErrorMessage.java
+++ src/main/java/menu/constant/ErrorMessage.java
@@ -3,10 +3,14 @@ package menu.constant;
 public enum ErrorMessage {
     EXCEPTION_PREFIX("[ERROR] "),
 
-    INVALID_NAME_LENGTH("코치 이름은 2글자 이상 4글자 이하 이여야 합니다."),
-    COACH_COUNT_TOO_SHORT("코치는 최소 2명 이상 입력 해야 합니다."),
-    COACH_COUNT_TOO_LONG("코치는 최대 5명 이하 입력 해야 합니다."),
-    NAME_DUPLICATED("코치 이름이 중복 입력 되었 습니다.");
+    INVALID_NAME_LENGTH("코치 이름은 2글자 이상 4글자 이하이여야 합니다."),
+    COACH_COUNT_TOO_SHORT("코치는 최소 2명 이상 입력해야 합니다."),
+    COACH_COUNT_TOO_LONG("코치는 최대 5명 이하 입력해야 합니다."),
+    NAME_DUPLICATED("코치 이름이 중복 입력 되었습니다."),
+
+    MENU_DUPLICATED("메뉴가 중복 입력 되었습니다."),
+
+    MENU_NOT_EXIST("존재 하지 않는 메뉴가 입력 되었습니다.");
 
     private final String message;
 
--- src/main/java/menu/constant/GeneralMessage.java
+++ src/main/java/menu/constant/GeneralMessage.java
@@ -4,7 +4,7 @@ public enum GeneralMessage {
     NOTIFY_START("점심 메뉴 추천을 시작합니다."),
     NOTIFY_RESULT("메뉴 추천 결과입니다."),
     NOTIFY_DAY_OF_WEEK("[ 구분 | 월요일 | 화요일 | 수요일 | 목요일 | 금요일 ]"),
-    NOTIFY_CATEGORY("[ 카테고리 | 한식 | 한식 | 일식 | 중식 | 아시안 ]"),
+    NOTIFY_CATEGORY("[ 카테고리 | %s | %s | %s | %s | %s ]"),
     NOTIFY_NAME_AND_MENUS("[ %s | %s | %s | %s | %s | %s ]"),
     NOTIFY_FINISH("추천을 완료했습니다."),
 
