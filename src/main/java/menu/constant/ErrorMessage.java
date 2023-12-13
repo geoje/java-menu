137efb2ee65e3072ec93aff3bbcdb01b984608e9
Gyeongho Yang <chchch1213@naver.com>
2023-12-13 21:39:58 +0900

    refactor(const): 에러 메세지의 공백 추가

--- src/main/java/menu/constant/ErrorMessage.java
+++ src/main/java/menu/constant/ErrorMessage.java
@@ -4,9 +4,9 @@ public enum ErrorMessage {
     EXCEPTION_PREFIX("[ERROR] "),
 
     INVALID_NAME_LENGTH("코치 이름은 2글자 이상 4글자 이하 이여야 합니다."),
-    COACH_COUNT_TOO_SHORT("코치는 최소 2명 이상 입력해야 합니다."),
-    COACH_COUNT_TOO_LONG("코치는 최대 5명 이하 입력해야 합니다."),
-    NAME_DUPLICATED("코치 이름이 중복 입력 되었습니다.");
+    COACH_COUNT_TOO_SHORT("코치는 최소 2명 이상 입력 해야 합니다."),
+    COACH_COUNT_TOO_LONG("코치는 최대 5명 이하 입력 해야 합니다."),
+    NAME_DUPLICATED("코치 이름이 중복 입력 되었 습니다.");
 
     private final String message;
 
